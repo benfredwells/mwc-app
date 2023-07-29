5da4c9a853552c13440f8d226e632cc714429575
Ben Wells <benfred@mjaw.com>
2023-07-29 13:37:41 +1000

    Got custom element going

--- src/drawing.js
+++ src/drawing.js
@@ -1,1 +1,9 @@
-import { LitElement, html } from 'lit-element';
+import {LitElement, html} from 'lit';
+
+class MwcAppDrawing extends LitElement {
+    render() {
+        return html`<p>hello</p>`;
+    }
+}
+
+customElements.define('mwc-app-drawing', MwcAppDrawing);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import '@material/web/select/outlined-select';
 import '@material/web/select/filled-select.js';
 import '@material/web/select/select-option.js';
+import './drawing.js';

a53e3c716f5e0f860168704f5c23c3e6ced83208
Ben Wells <benfred@mjaw.com>
2023-07-31 15:02:19 +1000

    Draws something now...

--- src/drawing.js
+++ src/drawing.js
@@ -8,12 +8,41 @@ class MwcAppDrawing extends LitElement {
 
   constructor() {
     super();
-    this.colour = "green";
-    this.shape = "square";
+    this.colour = "";
+    this.shape = "";
+  }
+
+  get _canvas() {
+    return this.renderRoot?.querySelector('canvas') ?? null;
   }
 
   render() {
-    return html`<p>${this.colour} ${this.shape}</p>`;
+    return html`<canvas id="canvas" width=100 height=100></canvas>`;
+  }
+
+  firstUpdated() {
+    this.paint();
+  }
+
+  shouldUpdate() {
+    this.paint();
+    return (this._canvas == null);
+  }
+
+  paint() {
+    if (!this._canvas)
+      return;
+
+    var ctx = this._canvas.getContext("2d");
+    ctx.save();
+    ctx.clearRect(0, 0, 100, 100);
+
+    if (this.colour != "" && this.shape != "") {
+      ctx.fillStyle = this.colour;
+      ctx.fillRect(0, 0, 100, 100);
+    }
+
+    ctx.restore();
   }
 }
 

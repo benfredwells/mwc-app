9aa64cb9df608c8996080bbb06050796c5301c1c
Ben Wells <benfred@mjaw.com>
2023-08-19 20:45:19 -0700

    Use rollup plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'tsc-out/index.js',
+  input: 'src/index.ts',
   output: {
     dir: 'dist',
     format: 'cjs',
     sourcemap: true
   },
-  plugins: [nodeResolve()]
+  plugins: [typescript(), nodeResolve()]
 };
